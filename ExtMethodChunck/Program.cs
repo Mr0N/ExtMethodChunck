83d827176497ba24ac5be3fb7ca4eebd72032816
new_user <new_user@gmail.com>
2022-09-25 12:33:45 +0300

    Добавьте файлы проекта.

---
+++ ExtMethodChunck/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+
+var result = Enumerable.Range(0, 100).ChunckLazy(12);
+
+foreach (var collection in result)
+{
+    Console.WriteLine(new String('-',50));
+    foreach (var item in collection)
+    {
+        Console.Write("|"+item+"|");
+    }
+    Console.WriteLine();
+}
+Console.ReadKey();
+
+
+
+
+static class EnumeratorExt
+{
+    public static IEnumerable<IEnumerable<T>> ChunckLazy<T>(this IEnumerable<T> enumerable,int size)
+    {
+        int count = 0;
+        return enumerable.GroupBy(a =>
+        {
+            count++;
+            if (count >= size)
+                count = 0;
+            return count;
+        }).Select(a => a.AsEnumerable());
+    }
+
+}
